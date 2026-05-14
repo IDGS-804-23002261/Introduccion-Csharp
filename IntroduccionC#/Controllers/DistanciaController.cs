b2645baac2dd4cd81457edbc9ba96517d8e630d9
IDGS-804-23002261 <anahigomezz2810@gmail.com>
2026-05-14 09:45:22 -0600

    Distancia entre dos Puntos 13/05/26

---
+++ IntroduccionC#/Controllers/DistanciaController.cs
@@ -0,0 +1,33 @@
+﻿using IntroduccionC_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionC_.Controllers
+{
+    public class DistanciaController : Controller
+    {
+        public static List<DistanciaEntreDosPuntos>
+        listaDistancias = new List<DistanciaEntreDosPuntos>();
+
+
+        [HttpGet]
+        public ActionResult Distancia()
+        {
+            return View("Distancia", listaDistancias);
+        }
+
+
+        [HttpPost]
+        public ActionResult Distancia(DistanciaEntreDosPuntos dis)
+        {
+            dis.CalcularDistancia();
+
+            listaDistancias.Add(dis);
+
+            return View("Distancia", listaDistancias);
+        }
+    }
+}
