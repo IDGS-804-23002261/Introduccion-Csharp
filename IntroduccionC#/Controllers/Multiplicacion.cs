b9d1ab87d5845835f24b0144959f4a81927796e6
IDGS-804-23002261 <anahigomezz2810@gmail.com>
2026-05-14 09:47:37 -0600

    Multiplicacion a*b 14/05/26

---
+++ IntroduccionC#/Controllers/Multiplicacion.cs
@@ -0,0 +1,33 @@
+﻿using IntroduccionC_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionC_.Controllers
+{
+    public class MultiplicacionController : Controller
+    {
+        public static List<Multiplicacion>
+        listaMultiplicacion = new List<Multiplicacion>();
+
+
+        [HttpGet]
+        public ActionResult Multiplicacion()
+        {
+            return View(listaMultiplicacion);
+        }
+
+
+        [HttpPost]
+        public ActionResult Multiplicacion(Multiplicacion mul)
+        {
+            mul.Multiplicar();
+
+            listaMultiplicacion.Add(mul);
+
+            return View(listaMultiplicacion);
+        }
+    }
+}
---
+++ IntroduccionC#/Models/Multiplicacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionC_.Models
+{
+    public class Multiplicacion
+    {
+        public int Num1 { get; set; }
+
+        public int Num2 { get; set; }
+
+        public int Resultado { get; set; }
+
+        public string Proceso { get; set; }
+
+
+        public void Multiplicar()
+        {
+            Resultado = 0;
+
+            Proceso = "";
+
+            for (int i = 1; i <= Num1; i++)
+            {
+                Resultado += Num2;
+
+                Proceso += Num2;
+
+                if (i < Num1)
+                {
+                    Proceso += "+";
+                }
+            }
+        }
+    }
+}
